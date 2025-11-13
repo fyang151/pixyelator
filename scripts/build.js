8e66ba64c42fe6bd80032164fe91cf138a833333
fyang151 <fyang151@gmail.com>
2025-11-13 01:54:24 -0800

    Add minify script.

---
+++ scripts/build.js
@@ -0,0 +1,58 @@
+import { minify } from "terser";
+import fs from "fs/promises";
+import { fileURLToPath } from "url";
+import { dirname, join, resolve } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const projectRoot = resolve(__dirname, "..");
+const distDir = join(projectRoot, "dist");
+
+const terserOptions = {
+  compress: {
+    passes: 2,
+  },
+  mangle: {
+    toplevel: false,
+  },
+  format: {
+    comments: false,
+  },
+};
+
+async function ensureDistDir() {
+  try {
+    await fs.mkdir(distDir);
+  } catch (err) {
+    if (err.code !== "EEXIST") {
+      throw err;
+    }
+  }
+}
+
+async function build() {
+  await ensureDistDir();
+
+  const indexPath = join(projectRoot, "index.js");
+  const indexCode = await fs.readFile(indexPath, "utf8");
+  const indexMinified = await minify(indexCode, terserOptions);
+
+  const indexMinifiedCode = indexMinified.code.replace(
+    /new URL\("innerWorker\.js"/g,
+    'new URL("innerWorker.min.js"'
+  );
+
+  await fs.writeFile(join(distDir, "index.min.js"), indexMinifiedCode);
+
+  const workerPath = join(projectRoot, "innerWorker.js");
+  const workerCode = await fs.readFile(workerPath, "utf8");
+  const workerMinified = await minify(workerCode, terserOptions);
+
+  await fs.writeFile(
+    join(distDir, "innerWorker.min.js"),
+    workerMinified.code
+  );
+}
+
+build().catch(console.error);
